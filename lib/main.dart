3d008f51c57c98dc42970a6771d476f01f5dedb0
Gamal Azzam <gamalazzam05@gmail.com>
2026-01-15 07:23:36 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/home_view.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      tools: [...DevicePreview.defaultTools],
+      builder: (context) => const ResponsiveDashboard(),
+    ),
+  );
+}
+
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeView());
+  }
+}
